e544bc6e5f9f84e788cd82077eb58e15b661320b
TaimoorSaeed <taimoor.dev@gmail.com>
2022-11-23 18:53:54 +0500

    Day 02

---
+++ Dart/bin/22-11-2022/main.dart
@@ -0,0 +1,23 @@
+void main() {
+  // Write a program to print "Welcome to flutter Mobile application development"?
+  print("Welcome to flutter Mobile application development");
+
+// Write a program to perform all arithemetic operations?
+  var a = 10;
+  var b = 20;
+
+  print(a + b);
+  print(a - b);
+  print(a * b);
+  print(a / b);
+
+// Write a program to multiply three decimal number?
+  var c = 0.3;
+  var d = 0.5;
+  var e = 0.6;
+
+  print(c * d * e);
+
+// Differentiate between variable and dynamic?
+// For dynamic you can change the type of it later but not for var. You can change the value but not the type for var
+}
