75fd6c0052e0e256c63d2f1217f77a656c6052fd
TaimoorSaeed <taimoor.dev@gmail.com>
2022-12-05 20:47:42 +0500

    fix: changes folder names and add last lecture assignments

---
+++ Dart/lib/Lecture08/main.dart
@@ -0,0 +1,18 @@
+void main() {
+  // int count = 1;
+  // while (count <= 10) {
+  //   print("$count");
+  //   count++;
+  // }
+
+  // int count = 10;
+  // while (count >= 0) {
+  //   print("$count");
+  //   count--;
+  // }
+
+  int count = 10;
+  for (var i = 1; i <= count; i++) {
+    print("$i");
+  }
+}
